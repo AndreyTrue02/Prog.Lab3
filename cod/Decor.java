ec80ce282e1e03c365e9f49fec8056e3e668be00
AndreyTrue02 <73782641+AndreyTrue02@users.noreply.github.com>
2020-12-20 17:12:55 +0300

    Add files via upload

---
+++ cod/Decor.java
@@ -0,0 +1,11 @@
+public class Decor  {
+    private String Stairs;
+    public String getStairs() {
+        return " Лестница";
+    }
+    private String Window;
+    public String getWindow() {
+        return " Окно";
+    }
+ }
+
